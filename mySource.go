c461c08abed2eb5eebe78bf712f9c7d07d54656d
bakhi <bakhi@purdue.edu>
2016-10-10 21:51:07 -0400

    1

---
+++ mySource.go
@@ -0,0 +1,73 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"gopkg.in/sensorbee/sensorbee.v0/bql"
+	"gopkg.in/sensorbee/sensorbee.v0/core"
+	"gopkg.in/sensorbee/sensorbee.v0/data"
+)
+
+type SensorData struct {
+	ID    string
+	value int
+}
+
+type SourceCreator struct {
+	interval time.Duration
+}
+
+func (t SensorData) MakeData(name string, min, max int) error {
+	t.ID = name
+	t.value = rand.Intn(max - min)
+	return nil
+}
+
+func SimulatedSensor(name string, min, max int) []byte {
+	buf := bytes.NewBuffer(nil) // create empty buffer
+	value := rand.Intn(max - min)
+	buf.WriteString(name + ": " + strconv.Itoa(value))
+	//	value := rand.Intn(max+min) - min
+	//	tuple := map[string]int{name: value}
+	//	tuple := map[string]int{"name": name, "value": value}
+	//	tupleM, _ := json.Marshal(tuple)
+	//	return string(tupleM)v
+	return buf.Bytes()
+}
+
+func (s SourceCreator) GenerateStream(ctx *core.Context, w core.Writer) error {
+	tuple := new(SensorData)
+	for {
+		tuple.MakeData("temperature", 0, 30)
+		t := core.NewTuple(data.Map{tuple.ID: data.Int(tuple.value)})
+
+		if err := w.Write(ctx, t); err != nil {
+			return err
+		}
+		fmt.Println(tuple)
+		time.Sleep(s.interval)
+	}
+}
+
+func (s *SourceCreator) CreateMySource(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (core.Source, error) {
+	interval := 1 * time.Microsecond
+	if v, ok := params["interval"]; ok {
+		i, err := data.ToDuration(v)
+		if err != nil {
+			return nil, err
+		}
+		interval = i
+	}
+	return core.ImplementSourceStop(&SourceCreator{
+		interval: interval,
+	}), nil
+
+}
+
+func (s *SourceCreator) Stop(ctx *core.Context) error {
+	return nil
+}
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,7 +1,15 @@
-package main
+package plugin
 
-import "fmt"
+import (
+	"github.com/bakhi/test"
+	"github.com/bakhi/udsfs"
+	"gopkg.in/sensorbee/sensorbee.v0/bql"
+)
 
-func main() {
-	fmt.Println("vim-go")
+func init() {
+	bql.MustRegisterGlobalSourceCreator("lorem", bql.SourceCreatorFunc(udsfs.CreateLoremSource))
+	bql.MustRegisterGlobalSourceCreator("src", bql.SourceCreatorFunc(test.CreateMySource))
+	//	bql.MustRegisterGlobalSourceCreator("lorem", bql.SourceCreatorFunc(udsfs.CreateLoremSource))
+	//	udf.MustRegisterGlobalUDSFCreator("word_splitter", udf.MustConvertToUDSFCreator(udsfs.CreateWordSplitter))
+	//	udf.MustRegisterGlobalUDSFCreator("ticker", udf.MustConvertToUDSFCreator(udsfs.CreateTicker))
 }
